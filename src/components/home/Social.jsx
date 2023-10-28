7994f03b41a8c3ef09433ba2ab55c66c81a7e7cb
Xolani <140137794+XolaniLan@users.noreply.github.com>
2023-10-28 15:50:27 +0200

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Social = () => {
   return (
     <div className='home__social'>
-      <a href='https://dribbble.com/' className='home__social-icon'>
+      <a href='https://www.linkedin.com/in/xolani-shongwe-a209a7235' className='home__social-icon'>
         <i className='uil uil-linkedin'></i>
       </a>
 
-      <a href='https://github.com/' className='home__social-icon'>
+      <a href='https://github.com/XolaniLan' className='home__social-icon'>
         <i className='uil uil-github-alt'></i>
       </a>
     </div>
