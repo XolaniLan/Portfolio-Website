e7ed68461e3395307d672350285e26612d4fbbc5
XolaniLan <lanshongwe10@gmail.com>
2023-10-28 15:28:49 +0200

    first commit

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,62 @@
+import Work1 from '../../assets/work1.jpg';
+import Work2 from '../../assets/work2.jpg';
+import Work3 from '../../assets/work3.jpg';
+import Work4 from '../../assets/work4.jpg';
+// import Work5 from '../../assets/work5.jpg';
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: 'Walxom Real Estate Webiste',
+    category: 'Website',
+    sourceCode: 'https://github.com/XolaniLan/Walxom-Real-Estate-Website/tree/main/Walxom%20Real%20Estate',
+    livePreview: 'https://walxom-real-estate-website-jg2c.vercel.app/'
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: 'Bank Application(xs, 1111/sd, 2222)',
+    category: 'Javascript Website',
+    sourceCode: 'https://github.com/XolaniLan/BankApplication/tree/main',
+    livePreview: 'https://bank-application-bay.vercel.app/'
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: 'Home Automation',
+    category: 'Website',
+    sourceCode: 'https://github.com/XolaniLan/SmartCastleHomeAutomation',
+    livePreview: 'https://smart-castle-home-automation.vercel.app/'
+
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: 'Restaurant Website',
+    category: 'app',
+    sourceCode: 'https://github.com/XolaniLan/Outeniqua-Grilli-Restaurant',
+    livePreview: 'https://outeniqua-grilli-restaurant.vercel.app/'
+  },
+  // {
+  //   id: 5,
+  //   image: Work5,
+  //   title: 'Web design',
+  //   category: 'Web',
+  // },
+];
+
+export const projectsNav = [
+  {
+    name: 'all',
+  },
+  // {
+  //   name: 'Web',
+  // },
+  // {
+  //   name: 'app',
+  // },
+  // {
+  //   name: 'design',
+  // },
+];
---
+++ src/components/work/WorkItems.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const WorkItems = ({ item }) => {
+  return (
+    <div className='work__card' key={item.id}>
+      <img src={item.image} alt='' className='work__img' />
+      <h3 className='work__title'>{item.title}</h3>
+      <a href={item.livePreview} className='work__button'>
+        Live Preview <i className='bx bx-right-arrow-alt work__button-icon'></i>
+      </a>
+      <a href={item.sourceCode} className='work__button'>
+        Source Code <i className='bx bx-right-arrow-alt work__button-icon'></i>
+      </a>
+    </div>
+  );
+};
+
+export default WorkItems;
