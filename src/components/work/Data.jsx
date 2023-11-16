1c85cc05b4ab3e4de91a1d2410eb9ccfb01e793f
Xolani <140137794+XolaniLan@users.noreply.github.com>
2023-11-16 15:25:31 +0200

    Update Data.jsx

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,24 +2,24 @@ import Work1 from '../../assets/work1.jpg';
 import Work2 from '../../assets/work2.jpg';
 import Work3 from '../../assets/work3.jpg';
 import Work4 from '../../assets/work4.jpg';
-// import Work5 from '../../assets/work5.jpg';
+import Work5 from '../../assets/work5.jpg';
 
 export const projectsData = [
   {
     id: 1,
+    image: Work5,
+    title: 'Recipe Application',
+    category: 'app',
+    sourceCode: 'https://github.com/XolaniLan/Recipo-Application',
+    livePreview: 'https://recipo-application.vercel.app/'
+  },
+  {
+    id: 2,
     image: Work1,
     title: 'Walxom Real Estate Webiste',
     category: 'Website',
     sourceCode: 'https://github.com/XolaniLan/Walxom-Real-Estate-Website/tree/main/Walxom%20Real%20Estate',
     livePreview: 'https://walxom-real-estate-website-jg2c.vercel.app/'
-  },
-  {
-    id: 2,
-    image: Work2,
-    title: 'Bank Application(xs, 1111/sd, 2222)',
-    category: 'Javascript Website',
-    sourceCode: 'https://github.com/XolaniLan/BankApplication/tree/main',
-    livePreview: 'https://bank-application-bay.vercel.app/'
   },
   {
     id: 3,
@@ -38,17 +38,20 @@ export const projectsData = [
     sourceCode: 'https://github.com/XolaniLan/Outeniqua-Grilli-Restaurant',
     livePreview: 'https://outeniqua-grilli-restaurant.vercel.app/'
   },
-  // {
-  //   id: 5,
-  //   image: Work5,
-  //   title: 'Web design',
-  //   category: 'Web',
-  // },
+  
+  {
+    id: 5,
+    image: Work2,
+    title: 'Bank Application(xs, 1111/sd, 2222)',
+    category: 'Javascript Website',
+    sourceCode: 'https://github.com/XolaniLan/BankApplication/tree/main',
+    livePreview: 'https://bank-application-bay.vercel.app/'
+  },
 ];
 
 export const projectsNav = [
   {
-    name: 'all',
+    name: 'HTML5, CSS3, JavaScript',
   },
   // {
   //   name: 'Web',
